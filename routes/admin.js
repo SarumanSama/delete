1fb5d77276189259a1c32aeccea23994cc8c3789
SᴀʀᴜᴍᴀɴSᴀᴍᴀ <94067379+SarumanSama@users.noreply.github.com>
2022-06-16 21:52:52 +0530

    Update admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const baixarmusica = require("../public/download");
 
 router.get( "/", (req, res) => {
-    res.sendFile(__dirname + "/pages/home.html");
+    res.sendFile(__dirname + "/pages/index.html");
 })
 
 router.post( "/baixar", (req, res) => {
